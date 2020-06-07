6a41fe49b6c54f04574fd94ce93741f1a6e4f8fb
GardenPi <pi@gardenpi>
2020-06-07 11:25:52 +0100

    Add cpu temp and logging

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import datetime
 import mcp
 import time
 import traceback
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="[{asctime}] {levelname} - {message}", style="{")
+LOG = logging.getLogger(__name__)
 
 def get_temperature_from_id(sensor_id: str):
     file = f"/sys/bus/w1/devices/28-00000{sensor_id}/w1_slave"
@@ -15,6 +19,24 @@ def get_temperature_from_id(sensor_id: str):
 
         return float(contents[start_pos + 2:-1]) / 1000
 
+def get_cpu_temp():
+    file = "/sys/class/thermal/thermal_zone0/temp"
+    with open(file, "r") as f:
+        contents = f.read()
+        return float(contents) / 1000
+
+
+def get_lux():
+    NUM_LUX_SAMPLES = 5
+
+    lux_samples = []
+
+    for _ in range(NUM_LUX_SAMPLES):
+        lux_samples.append(mcp.readmcp())
+        time.sleep(0.1)
+
+    return sum(lux_samples) / len(lux_samples)
+
 
 def main():
     sensors = ["4cba936","4cdf645","4ce8778"]
@@ -25,18 +47,20 @@ def main():
                 temp = get_temperature_from_id(sensor)
                 temps.append(temp)
 
-            lux = mcp.readmcp()
+            lux = get_lux()
 
-            entry = f"{datetime.datetime.now()},{lux},{','.join(str(x) for x in temps)}"
+            cpu_temp = get_cpu_temp()
+
+            entry = f"{datetime.datetime.now()},{lux},{','.join(str(x) for x in temps)},{cpu_temp}"
 
             with open("/home/pi/gardenpi.csv", "a") as f:
                 f.write(entry + "\n")
 
             time.sleep(60)
         except Exception as e:
-            with open("/home/pi/gardenpi.log", "a") as f:
-                f.write(traceback.format_exc())
+            LOG.exception(e)
 
 if __name__ == "__main__":
+    LOG.info("Starting GardenPi")
     main()
 
